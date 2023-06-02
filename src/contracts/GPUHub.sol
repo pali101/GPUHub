cf92ff4ee6df3b4ba3ff233ed88831dc151a77ee
Aashish Paliwal <aashish.paliwal@live.com>
2023-06-02 21:38:17 +0530

    added struct GPUListing and GPURequest to GPUHub contract

--- src/contracts/GPUHub.sol
+++ src/contracts/GPUHub.sol
@@ -2,5 +2,18 @@
 pragma solidity ^0.8.0;
 
 contract GPUHub {
+    struct GPUListing {
+        address provider;
+        string gpuModel;
+        uint256 capacity;
+        bool isAvailable;
+    }
+
+    struct GPURequest {
+        address requester;
+        string gpuModel;
+        uint256 duration;
+        bool isFulfilled;
+    }
 
 }
