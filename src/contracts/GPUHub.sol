d3f39a6a2c9ed217c737fe7bb97a1ea1af664b0e
Aashish Paliwal <aashish.paliwal@live.com>
2023-06-05 06:58:05 +0530

    Added createGPUListing function

--- src/contracts/GPUHub.sol
+++ src/contracts/GPUHub.sol
@@ -35,4 +35,22 @@ contract GPUHub {
         string gpuModel,
         uint256 duration
     );
+
+    /**
+     * @dev Create a new GPU listing.
+     * @param _gpuModel The GPU model.
+     * @param _capacity The capacity of the GPU.
+     */
+
+    function createGPUListing(string memory _gpuModel, uint256 _capacity, true) external {
+        require(_capacity > 0, "Capacity must be greater than 0");
+        gpuListings[listingCount] = GPUListing(
+            msg.sender,
+            _gpuModel,
+            _capacity,
+            true
+        );
+        emit GPUListingCreated(listingCount, msg.sender, _gpuModel, _capacity);
+        listingCount++;
+    }
 }
