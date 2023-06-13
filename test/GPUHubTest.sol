16d8734d8d8358e9e4d1a257f3226bacd494c72a
Aashish Paliwal <aashish.paliwal@live.com>
2023-06-13 21:17:01 +0530

    Added test cases in test/GPUHubTest.sol for getGPUListing function

--- test/GPUHubTest.sol
+++ test/GPUHubTest.sol
@@ -28,33 +28,20 @@ contract GPUHubTest {
         Assert.equal(isAvailable, true, "Incorrect availability");
     }
 
-    // function testCreateGPUListing() public {
-    //     uint256 listingId = 0;
-    //     string memory gpuModel = "RTX 3080";
-    //     uint256 capacity = 8;
+    function testGetGPUListing() public {
+        uint256 listingId = 0;
+        string memory gpuModel = "RTX 3080";
+        uint256 capacity = 8;
+        uint256 price = 10;
 
-    //     gpuHub.createGPUListing(gpuModel, capacity);
+        gpuHub.createGPUListing(gpuModel, capacity, price);
 
-    //     (address provider, string memory actualGpuModel, uint256 actualCapacity, bool isAvailable) = gpuHub.getGPUListing(listingId);
+        (address provider, string memory actualGpuModel, uint256 actualCapacity, uint256 actualPrice, bool isAvailable) = gpuHub.getGPUListing(listingId);
 
-    //     Assert.equal(provider, address(this), "Incorrect provider address");
-    //     Assert.equal(actualGpuModel, gpuModel, "Incorrect GPU model");
-    //     Assert.equal(actualCapacity, capacity, "Incorrect capacity");
-    //     Assert.equal(isAvailable, true, "Incorrect availability");
-    // }
-
-    // function testSubmitGPURequest() public {
-    //     uint256 requestId = 0;
-    //     string memory gpuModel = "RTX 3080";
-    //     uint256 duration = 24;
-
-    //     gpuHub.submitGPURequest(gpuModel, duration);
-
-    //     (address requester, string memory actualGpuModel, uint256 actualDuration, bool isFulfilled) = gpuHub.getGPURequest(requestId);
-
-    //     Assert.equal(requester, address(this), "Incorrect requester address");
-    //     Assert.equal(actualGpuModel, gpuModel, "Incorrect GPU model");
-    //     Assert.equal(actualDuration, duration, "Incorrect duration");
-    //     Assert.equal(isFulfilled, false, "Incorrect fulfillment status");
-    // }
+        Assert.equal(provider, address(this), "Incorrect provider address");
+        Assert.equal(actualGpuModel, gpuModel, "Incorrect GPU model");
+        Assert.equal(actualCapacity, capacity, "Incorrect capacity");
+        Assert.equal(actualPrice, price, "Incorrect price");
+        Assert.equal(isAvailable, true, "Incorrect availability");
+    }
 }
