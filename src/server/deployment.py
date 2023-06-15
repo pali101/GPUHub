10b3a96eaa9f18d4077ec2c22d63e24cd3a3e555
Aashish Paliwal <aashish.paliwal@live.com>
2023-06-15 15:27:02 +0530

    Refactored deployment.py for improved readability and added contract deployment functionality

--- src/server/deployment.py
+++ src/server/deployment.py
@@ -1,4 +1,4 @@
-import web3
+from web3 import web3
 from solcx import compile_standard, install_solc
 import json
 
@@ -25,3 +25,38 @@ print(compiled_sol)
 
 with open("compiled_code.json", "w") as file:
     json.dump(compiled_sol, file, indent=4)
+
+bytecode = compiled_sol["contracts"]["GPUHub.sol"]["GPUHub"]["evm"]["bytecode"][
+    "object"
+]
+abi = json.loads(compiled_sol["contracts"]["GPUHub.sol"]["GPUHub"]["metadata"])[
+    "output"
+]["abi"]
+
+# connect to ganache
+w3 = web3.Web3(web3.HTTPProvider("http://127.0.0.1:8545"))
+chain_id = 1337
+address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+# Remove this private key after testing and for production
+private_key = "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d"
+
+# create contract 
+contract_list = w3.eth.contract(abi=abi, bytecode=bytecode)
+nonce = w3.eth.getTransactionCount(address)
+
+# build txn
+txn = contract_list.constructor().buildTransaction({
+    "chainId": chain_id,
+    "gasPrice": w3.eth.gasPrice,
+    "from": address,
+    "nonce": nonce,
+}
+
+# sign txn
+signed_txn = w3.eth.account.sign_transaction(txn, private_key=private_key)
+print("Deploying contract...")
+# send txn
+txn_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+print("Waiting for contract to be deployed...")
+txn_receipt = w3.eth.wait_for_transaction_receipt(txn_hash)
+print("Contract deployed to {0}".format(txn_receipt.contractAddress))
